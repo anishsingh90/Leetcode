9dec3436b9cd71d5efc9d8e1ed469d840746d611
Anish Kumar <132129265+anishsingh90@users.noreply.github.com>
2025-02-25 12:29:26 +0530

    Time: 0 ms (100%), Space: 12 MB (5.11%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,44 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        if (nums.size() <= 1) return;
+        std::vector<int> temp(nums.size());
+        mergeSort(nums, temp, 0, nums.size() - 1);
+    }
+
+private:
+    void mergeSort(vector<int>& nums, vector<int>& temp, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(nums, temp, left, mid);
+            mergeSort(nums, temp, mid + 1, right);
+            merge(nums, temp, left, mid, right);
+        }
+    }
+
+    void merge(vector<int>& nums, vector<int>& temp, int left, int mid, int right) {
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+
+        while (i <= mid && j <= right) {
+            if (nums[i] <= nums[j]) {
+                temp[k++] = nums[i++];
+            } else {
+                temp[k++] = nums[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = nums[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = nums[j++];
+        }
+
+        for (int i = left; i <= right; i++) {
+            nums[i] = temp[i];
+        }
     }
 };
